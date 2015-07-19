60c2aa2f3b2f70538d44e0db0c8170c3e33a1c79
benywon <1138274167@qq.com>
2015-07-19 14:29:44 +0800

    更改了类别的相关

---
+++ src/main/ErrorAndWarn.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by benywon on 2015/7/19.
+ */
+public class ErrorAndWarn {
+    public final static int 成功=0;
+    public final static int 在找最近的时候没有目标=1;//就是最近的公园 没有识别出公园 这一类的
+
+}
